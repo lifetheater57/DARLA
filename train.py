fa7d65ab16a256cf276447c167afd4233b52143c
melisandeteng <melisande.teng@student.ecp.fr>
2020-12-04 15:39:31 -0500

    WIP - do not use

--- train.py
+++ train.py
@@ -1,4 +1,142 @@
-import torch
+from pathlib import Path
+from time import time, sleep
+import os
+import yaml
+from addict import Dict
+import argparse
+from comet_ml import Experiment, ExistingExperiment
+from data.data import *
+from utils.utils import env_to_path, copy_run_files, flatten_opts
+from dae.dae import DAE
+from beta_vae.beta_vae import BetaVAE
+
+
+def main(opts):
+    """
+    python train.py --config path_to_config.yaml 
+
+    """
+
+    # -----------------------------
+    # -----  Parse arguments  -----
+    # -----------------------------
+
+    parser = argparse.ArgumentParser(description="train model")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="./config/defaults.yaml",
+        help="path to config file",
+    )
+    parser.add_argument(
+        "--no_comet", type=bool, action="store_true", help="launch comet exp or not"
+    )
+    parser.add_argument(
+        "--comet_tags", type=str, default=None, help="tags for comet exp"
+    )
+    args = parser.parse_args()
+    # -----------------------
+    # -----  Load opts  -----
+    # -----------------------
+
+    with open(args.config, "r") as f:
+        opts = yaml.safe_load(f)
+
+    opts = Dict(opts)
+    opts.jobID = os.environ.get("SLURM_JOBID")
+    opts.output_path = str(env_to_path(opts.output_path))
+    print("Config output_path:", opts.output_path)
+
+    exp = comet_previous_id = None
+    # create output_path if it doesn't exist
+    Path(opts.output_path).mkdir(parents=True, exist_ok=True)
+
+    # Copy the opts's sbatch_file to output_path
+    copy_run_files(opts)
+
+    if not args.no_comet:
+        # ----------------------------------
+        # -----  Set Comet Experiment  -----
+        # ----------------------------------
+        if exp is None:
+            # Create new experiment
+            print("Starting new experiment")
+            exp = Experiment(project_name="DARLA", **comet_kwargs)
+            exp.log_asset_folder(
+                str(Path(__file__).parent / "DARLA"),
+                recursive=True,
+                log_file_name=True,
+            )
+            exp.log_asset(str(Path(__file__)))
+
+        # log tags
+        if args.comet_tags:
+            tags = set()
+            if args.comet_tags:
+                tags.update(args.comet_tags)
+            opts.comet.tags = list(tags)
+            print("Logging to comet.ml with tags", opts.comet.tags)
+            exp.add_tags(opts.comet.tags)
+
+        # Log all opts
+        exp.log_parameters(flatten_opts(opts))
+
+        # allow some time for comet to get its url
+        sleep(1)
+
+        # Save comet exp url
+        url_path = Path(opts.output_path) / "comet_url.txt"
+        with open(url_path, "w") as f:
+            f.write(exp.url)
+
+        # Save config file
+        opts_path = Path(opts.output_path) / "opts.yaml"
+        with (opts_path).open("w") as f:
+            yaml.safe_dump(opts.to_dict(), f)
+
+    print("Running model in", opts.output_path)
+
+    # -------------------
+    # -----  Train  -----
+    # -------------------
+
+    loader = get_loader(opts, "train")
+
+    # TODO add get_shape
+
+    dae = DAE(
+        opts.data.n_obs,
+        opts.num_epochs,
+        opts.data.loaders.batch_size,
+        opts.dae_lr,
+        opts.save_iter,
+        shape,
+    )
+    beta_vae = BetaVAE(
+        opts.data.n_obs,
+        num_epochs,
+        opts.data.loaders.batch_size,
+        opts.betavae_lr,
+        opts.beta,
+        opts.save_iter,
+        shape,
+    )
+
+    # TODO  Add training
+
+    # -----------------------------
+    # -----  End of training  -----
+    # -----------------------------
+
+    print("Done training")
+    # kill_job(opts.jobID)
+
+
+if __name__ == "__main__":
+
+    main()
+
+"""import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -45,3 +183,4 @@ dae.train(history)
 
 # train ß-VAE
 beta_vae.train(history, dae)
+"""
