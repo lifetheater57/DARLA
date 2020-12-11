b97bf440f7b93c1a2118d63ee692a9a690eb1039
melisandeteng <melisande.teng@student.ecp.fr>
2020-12-11 16:16:33 -0500

    fix dims

--- utils/latent_space.py
+++ utils/latent_space.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 #%%
 
+
 def getLatentSpaceStats(encoder, data, return_dataframe=False):
     # Encode the data in latent space
     encoded_data = encoder.encode(data)
@@ -11,21 +12,26 @@ def getLatentSpaceStats(encoder, data, return_dataframe=False):
     # Describe the latent variables
     if return_dataframe:
         return df
-    else: 
+    else:
         return df.describe()
 
+
 #%%
+
 
 def traversals(decoder, shape, dimensions, bounds, num_samples, state):
     # Initializing size attributes and figure
     n_dims = len(dimensions)
-    width, height, num_channels = shape[1], shape[2], shape[0]
+    width, height, num_channels = shape[2], shape[1], shape[0]
     figure = np.zeros((width * n_dims, height * num_samples, num_channels))
-    
-    assert bounds.shape == (2) or bounds.shape == (n_dims, 2), 
-        "cannot broadcast bounds size " + str(bounds.shape) + 
-        " with dimensions size " + str(dimensions.shape)
-    
+
+    assert bounds.shape == (2) or bounds.shape == (n_dims, 2), (
+        "cannot broadcast bounds size "
+        + str(bounds.shape)
+        + " with dimensions size "
+        + str(dimensions.shape)
+    )
+
     # Creating values for the sampling
     if len(bounds.shape) == 1:
         min_val = bounds[0]
@@ -35,13 +41,13 @@ def traversals(decoder, shape, dimensions, bounds, num_samples, state):
         columns = []
         for min_val, max_val in bounds:
             columns.append(np.linspace(min_val, max_val, num_samples)[::-1])
-    
+
     # Generating images
     for i, dim in enumerate(dimensions):
         z_samples = np.array([state.copy()] * len(columns[i]))
         for j, value in enumerate(columns[i]):
             z_samples[j, dim] = value
-        
+
         # Decode samples
         decoded_samples = decoder.predict(z_samples)
         # Transpose into a shape usable by matplotlib
@@ -51,7 +57,7 @@ def traversals(decoder, shape, dimensions, bounds, num_samples, state):
             (width, height * num_samples, num_channels)
         )
         # Assign to the colomn in the figure
-        figure[(i * width):((i + 1) * width)] = decoded_samples
+        figure[(i * width) : ((i + 1) * width)] = decoded_samples
 
     plt.figure(figsize=(12, 12))
     start_range = width // 2
@@ -61,8 +67,8 @@ def traversals(decoder, shape, dimensions, bounds, num_samples, state):
     sample_range_y = np.round(grid_y, 1)
     plt.xticks(pixel_range, sample_range_x)
     plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel('z - dim 1')
-    plt.ylabel('z - dim 2')
+    plt.xlabel("z - dim 1")
+    plt.ylabel("z - dim 2")
     # matplotlib.pyplot.imshow() needs a 2D array, or a 3D array with the third dimension being of shape 3 or 4!
     # So reshape if necessary
     fig_shape = np.shape(figure)
