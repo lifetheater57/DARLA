917afe0509d8f6668b442e3b0af55b93bc33a8c6
Étienne Boucher <etienneboucher@hotmail.ca>
2020-12-30 16:00:38 -0500

    Image normalization in loader was disabled.

--- data/transforms.py
+++ data/transforms.py
@@ -45,7 +45,7 @@ def get_transforms(opts):
     """
     last_transforms = [
         trsfs.ToTensor(),
-        trsfs.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        #trsfs.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 
     conf_transforms = []
